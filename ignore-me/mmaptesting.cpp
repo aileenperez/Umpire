b0d399ea6ecdaeb8e5bf0f6ef4008f1bd924c042
Aileen Perez <perez93@llnl.gov>
2020-06-24 13:45:08 -0500

    Saving

--- ignore-me/mmaptesting.cpp
+++ ignore-me/mmaptesting.cpp
@@ -18,15 +18,17 @@ public:
     void deallocate();
 
 private:
+    int file_counter;
     int fd;
     int truncated;
     int demap;
 };
 
 MapMemory::MapMemory()
-{   // Create a file 
-    fd = open("./aFile", O_RDWR | O_CREAT, S_IRWXU);
+{   // Create a file
+    fd = open("./file" + to_string(file_counter), O_RDWR | O_CREAT, S_IRWXU);
     if(fd == -1) { std::cerr << "Error: " << strerror(errno) << '\n'; }
+    file_counter = 1;
 
     // Set page size to computer specific
     SIZE = sysconf(_SC_PAGE_SIZE);
@@ -36,6 +38,8 @@ MapMemory::MapMemory()
 
     ADDRESS = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if(ADDRESS == MAP_FAILED) { std::cerr << "Error: " << strerror(errno) << '\n'; }
+
+    std::cout << "Allocated 1 page of memory. Size: " << SIZE << "\n";
 }
 
 MapMemory::~MapMemory()
@@ -45,8 +49,10 @@ MapMemory::~MapMemory()
 
 void MapMemory::allocate(int size)
 {
+
     // Set wanted page size to be the size of the length * the desired amount
-    SIZE = SIZE * (size + 1);
+    SIZE = (SIZE * size) + SIZE;
+    file_counter++;
 
     truncated = truncate("./aFile", SIZE);
     if(truncated == -1) { std::cerr << "Error: " << strerror(errno) << '\n'; }
@@ -73,6 +79,7 @@ int main()
     MapMemory map;
 
     std::cout << "Map Address: "<< map.ADDRESS << " Size: "<< map.SIZE << "\n";
+    // New File of allocation
     map.allocate(1);
     std::cout << "Map Address: "<< map.ADDRESS << " Size: "<< map.SIZE << "\n";
     return 0;
